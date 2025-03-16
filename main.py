63c7e0c8b3357896c12accef5a4e3d2ee5e70211
vasabi07 <vasanthanarulselvan@gmail.com>
2025-03-16 05:43:42 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print("Hello from bilbo-baggins!")
+
+
+if __name__ == "__main__":
+    main()
+"""make those 2 agents then create a main agent and then make them combined using command.
+create a machine control that decides the transfer and then try it out for 
+1. just websearch activity 
+2. ask the question together.
+3. add checkpointer and ask the question in couple of steps
+"""
