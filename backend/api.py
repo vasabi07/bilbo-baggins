7999909d7682024ea78ed3e028b9fe546a94f12f
vasabi07 <vasanthanarulselvan@gmail.com>
2025-03-26 20:12:25 +0530

    added some thread logic n planning for api endpoints in nodejs side

--- backend/api.py
+++ backend/api.py
@@ -1,10 +1,16 @@
-"""create an api endpoint for the main graph to contact FE"""
+"""create an api endpoint for the main graph to contact FE,
+use checkpointer and connect to a DB may be something like redis for faster retrieval
+
+"""
 from fastapi import FastAPI 
 from fastapi.middleware.cors import CORSMiddleware
-from langgraph.graph import MessagesState
-from langchain_core.messages import HumanMessage
-from pydantic import BaseModel
+from utils.types import Message
+from typing import TypedDict
+from typing_extensions import NotRequired
+import uuid 
 from main import graph
+
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -14,17 +20,22 @@ app.add_middleware(
     allow_headers=["*"], 
 )
 
-class Request_type(BaseModel):
-    # threadId: str
+class Request_type(TypedDict):
+    threadId: NotRequired[str]
     message: str
 
+def create_thread_id() -> str:
+    return str(uuid.uuid4())
 
 @app.post("/chat")
 async def chat(request: Request_type):
-    response =await graph.ainvoke({"messages": [HumanMessage(content=request.message)]})
+    if request.threadId == "":
+        request.threadId = create_thread_id()
+    #must add threadId logic with checkpointer from langgraph 
+    response =await graph.ainvoke({"messages": [Message(type="Human",content=request.message)]})
     print(response["messages"][-1].content)
     reply = response["messages"][-1].content
-    return {"reply": reply}
+    return {"threadId":request.threadId,"reply": reply}
 
 if __name__ == "__main__":
     import uvicorn
